a9f37b0a7253db9e4540832d8e4942b04984df16
yebink <yebin825@gmail.com>
2020-11-17 11:42:07 +0900

    feat: 리플렉션으로 여러 개 가져오기
     - 메소드, 생성자, 상위 클래스, 인터페이스, modifier

--- spring-di/src/main/java/com/ellyspace/springdi/App.java
+++ spring-di/src/main/java/com/ellyspace/springdi/App.java
@@ -1,5 +1,6 @@
 package com.ellyspace.springdi;
 
+import java.lang.reflect.Modifier;
 import java.util.Arrays;
 
 public class App {
@@ -16,12 +17,12 @@ public class App {
 
         //어떻게든 접근을 했다면...
         Arrays.stream(bookClass.getFields()).forEach(System.err::println); //public으로 정의된 d밖에 안 보인다.
-        System.err.println("====================");
+        System.err.println("==========1==========");
         Arrays.stream(bookClass.getDeclaredFields()).forEach(System.err::println); //다 가져온다.
 
-        System.err.println("====================");
+        System.err.println("==========2==========");
 
-        //인스턴스의 값을 가져오고 싶다?
+        //인스턴스의 필드 가져오기
         Arrays.stream(bookClass.getDeclaredFields()).forEach(field -> {
             try {
                 field.setAccessible(true);
@@ -30,6 +31,35 @@ public class App {
                 e.printStackTrace();
             }
         });
+        System.err.println("==========3==========");
+
+        //인스턴스의 메소드 가져오기
+        Arrays.stream(bookClass.getMethods()).forEach(System.err::println);
+        System.err.println("==========4==========");
+        Arrays.stream(bookClass.getDeclaredMethods()).forEach(System.err::println);
+        System.err.println("==========5==========");
+
+        //인스턴스의 생성자 가져오기
+        Arrays.stream(bookClass.getDeclaredConstructors()).forEach(System.err::println);
+        System.err.println("==========6==========");
+
+        //상위 클래스 가져오기
+        Class<? super MyBook> superclass = MyBook.class.getSuperclass();
+        System.err.println(superclass);
+        System.err.println("==========7==========");
+
+        //인터페이스 가져오기
+        Arrays.stream(MyBook.class.getInterfaces()).forEach(System.err::println);
+        System.err.println("==========8==========");
+
+        //modifier(ex.접근제어자) 확인하기
+        Arrays.stream(bookClass.getDeclaredFields()).forEach(field -> {
+            int modifiers = field.getModifiers();
+            System.err.println(modifiers);
+            System.err.printf("%s 의 접근제어자가 private: %s, public: %s, static: %s \n",
+                    field, Modifier.isPrivate(modifiers), Modifier.isPublic(modifiers), Modifier.isStatic(modifiers));
+        });
+        System.err.println("==========9==========");
 
     }
 }
