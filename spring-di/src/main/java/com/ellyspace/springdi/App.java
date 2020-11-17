4f92b6b338c67b8cfcc75bc311333364a359e267
yebink <yebin825@gmail.com>
2020-11-17 10:31:45 +0900

    feat: 리플렉션으로 인스턴스 접근

--- spring-di/src/main/java/com/ellyspace/springdi/App.java
+++ spring-di/src/main/java/com/ellyspace/springdi/App.java
@@ -1,7 +1,35 @@
 package com.ellyspace.springdi;
 
+import java.util.Arrays;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
+        //클래스에서 Class 접근하기
+        Class<Book> bookClass = Book.class;
+
+        //인스턴스에서 Class 접근하기
+        Book book = new Book();
+        Class<? extends Book> instanceWay = book.getClass();
+
+        //String으로 Class 접근하기
+        Class<?> stringWay = Class.forName("com.ellyspace.springdi.Book");
+
+        //어떻게든 접근을 했다면...
+        Arrays.stream(bookClass.getFields()).forEach(System.err::println); //public으로 정의된 d밖에 안 보인다.
+        System.err.println("====================");
+        Arrays.stream(bookClass.getDeclaredFields()).forEach(System.err::println); //다 가져온다.
+
+        System.err.println("====================");
+
+        //인스턴스의 값을 가져오고 싶다?
+        Arrays.stream(bookClass.getDeclaredFields()).forEach(field -> {
+            try {
+                field.setAccessible(true);
+                System.err.printf("%s 의 값 : %s \n", field, field.get(new Book())); //인스턴스가 있어야 한다.
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        });
 
     }
 }
