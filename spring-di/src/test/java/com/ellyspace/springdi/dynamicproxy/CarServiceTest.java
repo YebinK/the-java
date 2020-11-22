a8b844c71d2f1438e4c67681c8b4b2474fc08c0f
yebink <yebin825@gmail.com>
2020-11-23 02:44:54 +0900

    feat: 자바에서 Proxy 구현의 한계

--- spring-di/src/test/java/com/ellyspace/springdi/dynamicproxy/CarServiceTest.java
+++ spring-di/src/test/java/com/ellyspace/springdi/dynamicproxy/CarServiceTest.java
@@ -8,7 +8,7 @@ import java.lang.reflect.Proxy;
 
 class CarServiceTest {
 
-    //CarService의 프록시를 만든다.
+    //CarService의 프록시를 만든다. !!주의: 자바는 클래스가 아니라 인터페이스 기반의 프록시만 만들 수 있다. 그래서 DefaultCarService로는 못 만든다.
     CarService carService = (CarService) Proxy.newProxyInstance(CarService.class.getClassLoader(),
             new Class[]{CarService.class},
             new InvocationHandler() { //이 프록시의 메소드가 호출될 때 어떻게 처리할지 정의 (프록시의 부가기능 추가)
